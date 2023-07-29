49cb3405c2e7be3e546c18e507f27b7db6243ded
unknown <gandharvesolanki@gmail.com>
2023-07-29 19:55:07 +0530

    Implemented the functionality of sending message to the server and extracting message from the server in  main.js file

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ import socketio from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const ip = socketio(server);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
 //set Static folder
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', server => {
+    console.log("connection is success");
+})
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
